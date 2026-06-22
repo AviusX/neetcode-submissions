f0b325793d2a52416de79cce07145ed8be83d5cc
Hrijul Bhatnagar <55204295+AviusX@users.noreply.github.com>
2026-06-23 04:39:50 +0530

    Add: add-two-numbers - submission-11

---
+++ Data Structures & Algorithms/add-two-numbers/submission-11.kt
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class Solution {
+    fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+        when {
+            l1 == null && l2 != null -> return l2
+            l2 == null && l1 != null -> return l1
+            l1 == null && l2 == null -> return null
+        }
+
+        val dummy = ListNode(0)
+        var l1Curr = l1
+        var l2Curr = l2
+        var currentNode = dummy
+        var carry = 0
+
+        while (l1Curr != null || l2Curr != null || carry != 0) {
+            val x = l1Curr?.`val` ?: 0
+            val y = l2Curr?.`val` ?: 0
+            val sum = x + y + carry
+
+            carry = if (sum > 9) sum / 10 else 0
+            currentNode.next = ListNode(sum % 10)
+            currentNode = currentNode.next!!
+
+            l1Curr = l1Curr?.next
+            l2Curr = l2Curr?.next
+        }
+
+        return dummy.next
+    }
+}
