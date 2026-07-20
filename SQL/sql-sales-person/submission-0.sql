d7d47aa54d21376de116081b8521bec188ac5250
Hrijul Bhatnagar <55204295+AviusX@users.noreply.github.com>
2026-07-21 04:32:02 +0530

    Add: sql-sales-person - submission-0

---
+++ SQL/sql-sales-person/submission-0.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT sp.name
+FROM sales_person sp
+WHERE sp.sales_id NOT IN (
+    SELECT o.sales_id
+    FROM orders o
+    JOIN company c on c.com_id = o.com_id
+    WHERE c.name = 'CRIMSON'
+);
